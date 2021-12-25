0d594ca975ea2e8a4c2b413a71fa3a07f22a9deb
luca0898 <lucaguiraldello@hotmail.com>
2021-12-25 20:32:48 -0300

    Listando extrato

--- src/index.js
+++ src/index.js
@@ -29,4 +29,18 @@ app.post('/account', (request, response) => {
     return response.status(201).send();
 })
 
+app.get('/statement/:cpf', (request, response) => {
+    const { cpf } = request.params
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if (!customer){
+        return response.status(400).json({
+            error: "Customer don't exist!"
+        })
+    }
+
+    return response.json(customer.statement)
+})
+
 app.listen(3333, null, () => console.log('🚀 listening port 3333 ...'))
