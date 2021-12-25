f8e212eda5e4cb2ef056d593c50656d950ca7757
luca0898 <lucaguiraldello@hotmail.com>
2021-12-25 20:54:01 -0300

    Criando depósito na conta

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ app.use(express.json())
 
 let customers = []
 
-function verifyIfExistsAccountCPF(request, response, next){
-    const { cpf } = request.params
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers
 
     const customer = customers.find(customer => customer.cpf === cpf)
 
-    if (!customer){
+    if (!customer) {
         return response.status(400).json({
             error: "Customer not found!"
         })
@@ -39,15 +39,29 @@ app.post('/account', (request, response) => {
         cpf,
         name,
         id: uuidv4(),
-        statement: []
+        statements: []
     })
 
     return response.status(201).send();
 })
 
-app.get('/statement/:cpf', verifyIfExistsAccountCPF, (request, response) => {
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
 
-    return response.json(request.customer.statement)
+    return response.json(request.customer.statements)
+})
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+
+    const { body: { description, amount }, customer } = request;
+
+    customer.statements.push({
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit'
+    })
+
+    return response.status(201).json()
 })
 
 app.listen(3333, null, () => console.log('🚀 listening port 3333 ...'))
